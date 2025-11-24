49c973e6e88e4876a143e8dbc2004832dbd1ce2f
Abhijeet Rohilla <nishuinhurry@gmail.com>
2025-11-24 13:21:38 +0530

    netflix-gpt first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    < >
+      <h1 className="bg-gray-700 text-amber-100 text-3xl font-bold" >Welcome to Netflix GPT</h1>    
+    </>
+  )
+}
+
+export default App
